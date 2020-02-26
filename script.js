750f1cc2825a0255e5916f3640b516ecc73509af
Nathan Long <nrlong13@gmail.com>
2020-02-26 15:58:01 -0500

    updated js to create forms with save buttons.  time stamps inprogress

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
+//variables for creating calendar text areas and buttons
+
 let momentTime = $("#currentDay");
 let container = $(".container");
+
 
 
 // function to keep time updated actively
@@ -11,15 +14,29 @@ setInterval(update, 1000);
 
 // Generate hours of the day text fields and buttons
 
-for (let i=1; i < 25; i++){
-    let newDiv = document.createElement("div");
+for (let i=9; i < 19; i++){
+    let newDivWrapper = document.createElement("div")
+    let newInput = document.createElement("input");
+    let hourDiv = document.createElement("div");
     let newButton = document.createElement("button");
-    newDiv.setAttribute("id",  i);
-    newDiv.setAttribute("class", "dataEnter");
-    newDiv.textContent = "test" + i;
+
+    newDivWrapper.setAttribute("id", "container-fluid")
+    newDivWrapper.setAttribute("class", "dataEnter");
+    newInput.setAttribute("id",  "inputText");
+    hourDiv.setAttribute("class", "hourDiv");
     newButton.setAttribute("class", "btn saveBtn " + i);
-    newButton.innerHTML ='<i class="far fa-save fa-3x"></i>';
-    // newButton.textContent = "Save";
-    container.append(newDiv);
-    newDiv.append(newButton);
+    newButton.innerHTML ='<i class="far fa-save fa-2x"></i>';
+    container.append(newDivWrapper);
+    newDivWrapper.append(hourDiv, newInput, newButton);
+}
+
+let timeSet = document.querySelectorAll(".hourDiv");
+for (let x = 9; x < 19; x++){
+    
+    if (x > 12){
+        timeSet.forEach(element => {
+            element.textContent =  x - 12 + ":00"})
+        } else {timeSet.forEach(element => {
+            element.textContent =  x;})
+    }
 }
