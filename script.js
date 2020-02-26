511716267460e384fcf9e1e1bcf4e0175b3cf594
Nathan Long <nrlong13@gmail.com>
2020-02-25 21:03:19 -0500

    set loop to create divs for each day for input with corrisponding save buttons

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 let momentTime = $("#currentDay");
+let container = $(".container");
 
+
+// function to keep time updated actively
 function update(){
 momentTime.text(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"));
 }
 
 setInterval(update, 1000);
+
+// Generate hours of the day text fields and buttons
+
+for (let i=1; i < 25; i++){
+    let newDiv = document.createElement("div");
+    let newButton = document.createElement("button");
+    newDiv.setAttribute("id",  i);
+    newDiv.setAttribute("class", "dataEnter");
+    newDiv.textContent = "test" + i;
+    newButton.setAttribute("class", "btn saveBtn " + i);
+    newButton.textContent = "Save" + i;    
+    container.append(newDiv);
+    newDiv.append(newButton);
+}
