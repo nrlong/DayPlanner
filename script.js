24a7915820731c01c0d10a4c1bec3384f6fe6e64
Nathan Long <nrlong13@gmail.com>
2020-03-01 16:09:32 -0500

    js logic completed with all save functions and color switch for hours

--- script.js
+++ script.js
@@ -1,41 +1,115 @@
-//variables for creating calendar text areas and buttons
-
-let momentTime = $("#currentDay");
-let container = $(".container");
-
-
-
-// function to keep time updated actively
-function update(){
-momentTime.text(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"));
-}
-
-setInterval(update, 1000);
-
-// Generate hours of the day text fields and buttons
-
-for (let i=9; i < 19; i++){
-    let newDivWrapper = document.createElement("div")
-    let newInput = document.createElement("input");
-    let hourDiv = document.createElement("div");
-    let newButton = document.createElement("button");
-
-    newDivWrapper.setAttribute("id", "container-fluid")
-    newDivWrapper.setAttribute("class", "dataEnter");
-    newInput.setAttribute("id",  "inputText");
-    hourDiv.setAttribute("class", "hourDiv");
-    newButton.setAttribute("class", "btn saveBtn " + i);
-    newButton.innerHTML ='<i class="far fa-save fa-2x"></i>';
-    container.append(newDivWrapper);
-    newDivWrapper.append(hourDiv, newInput, newButton);
-
-    let timeSpan = $("<span>");
-    $(".hourDiv").append(timeSpan);
-
-    if (i > 12){
-        timeSpan.text(i - 12 + ":00");
-         } else { 
-             timeSpan.text(i + ":00");
-     }
+$(document).ready(function(){
     
-}
+    const test = false;
+    const now = moment().format("MMMM Do YYYY");
+
+
+    let nowHour24 = moment().format("H");
+    let nowHour12 = moment().format("h");
+
+    if(test){
+        nowHour24 = 12;
+        nowHour12 = 1;
+    }
+
+    let dateHeading = $("#currentDay");
+    dateHeading.text(now);
+
+    let storedPlans = JSON.parse(localStorage.getItem("storedPlans"));
+
+
+    if (storedPlans !== null){
+        plansArr = storedPlans;
+    }else{
+        plansArr = new Array(9);
+    }
+
+
+    let plannerDiv = $("#plans");
+    plannerDiv.empty();
+
+    if (test){console.log("current time", nowHour12);}
+
+
+//build daily schedule
+    for (let hour = 9; hour <= 17; hour++){
+        let index = hour - 9;
+
+        let rowDiv = $("<div>");
+        rowDiv.attr('class', 'row plannerRow');
+        rowDiv.attr('hour-index', hour);
+
+        let timeColDiv = $("<div>");
+        timeColDiv.add("class", "col-md-2");
+
+        const timeSpan = $("<span>");
+        timeSpan.attr("class", "timeBox");
+
+        let displayHour = 0;
+        let ampm = "";
+        if(hour > 12){
+            displayHour = hour - 12;
+            ampm = "pm";
+        }else{
+            displayHour = hour;
+            ampm ="am"
+        }
+
+        timeSpan.text(displayHour + ampm);
+
+        rowDiv.append(timeColDiv);
+        timeColDiv.append(timeSpan);
+
+        let dailyInput = $("<input>");
+        dailyInput.attr("id", "input-"+index);
+        dailyInput.attr("hour-index", index)
+        dailyInput.attr("type", "text");
+        dailyInput.attr("class","dailyPlan");
+
+        dailyInput.val(plansArr[index]);
+
+        let balanceDiv = $("<div>");
+        balanceDiv.attr("class", "col-md-9");
+
+        rowDiv.append(balanceDiv);
+        balanceDiv.append(dailyInput);
+
+        let saveDiv = $("<div>");
+        saveDiv.add("class", "col-md-1");
+
+        let saveBtn = $("<i>");
+        saveBtn.attr("id", "saveid"+index);
+        saveBtn.attr("save-id", index);
+        saveBtn.attr("class", "fa fa-save saveIcon");
+
+        rowDiv.append(saveDiv);
+        saveDiv.append(saveBtn);
+
+        rowColorChange(rowDiv, hour);
+
+        plannerDiv.append(rowDiv);
+    };
+
+    function rowColorChange(hourRow, hour){
+        if (hour < nowHour24){
+            hourRow.attr("class", "past");
+        }else if(hour > nowHour24){
+            hourRow.attr("class", "future");
+        }else{
+            hourRow.attr("class", "present");
+        }
+    }
+
+    $(document).on("click", "i", function(event){
+        event.preventDefault();
+
+        let index = $(this).attr("save-id");
+
+        let inputId = $("#input-"+index);
+        let value = inputId.val();
+        console.log(value);
+        plansArr[index] = value;
+
+        localStorage.setItem("storedPlans", JSON.stringify(plansArr));
+    });
+})
